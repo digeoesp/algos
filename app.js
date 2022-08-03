a728897e2c56ed595f9ad35f30f253215ef11d77
digeoesp <diegocrypto@outlook.com>
2022-08-02 21:09:29 -0700

    initial

---
+++ app.js
@@ -0,0 +1,18 @@
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let target = 8
+//Two Num Sum
+function twoNumberSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        firstNum = nums[i];
+        for (let j = i + 1; j < nums.length; j++) {
+            secondNum = nums[j];
+            if (firstNum + secondNum === target) {
+                return [firstNum, secondNum];
+            }
+        }
+    }
+    return []
+}
+
+console.log(twoNumberSum(` ${nums}, ${target}`));
+
